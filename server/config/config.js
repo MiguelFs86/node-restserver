5a5f4ba6009229534d0d77e4e87b0894cc3f50ac
MiguelFs86 <miguel.fuentes.sobrin@gmail.com>
2018-06-03 17:43:50 +0200

    Update mongo uri

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev'){
 	urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-	urlDB = 'mongodb://cafe-user:qwerty1234@ds247270.mlab.com:47270/udemy-nodejs-cafe'
+	//urlDB = 'mongodb://cafe-user:qwerty1234@ds247270.mlab.com:47270/udemy-nodejs-cafe';
+	urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
