42d3224f53efcb73e3a99901b38ed495b9e6e9ee
MiguelFs86 <miguel.fuentes.sobrin@gmail.com>
2018-06-03 20:33:59 +0200

    Login de usuarios y JWT

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bcrypt = require('bcrypt')
 const _ = require('underscore')
 const Usuario = require('../models/usuario')
+const { verificaToken, verificaAdminRole } = require('../middlewares/autenticacion')
 const app = express()
 
 
@@ -11,7 +12,7 @@ app.get('/', function (req, res) {
 })
 
 
-app.get('/usuario', function (req, res) {
+app.get('/usuario', verificaToken ,(req, res)=> {
 
 	let desde = req.query.desde || 0;
 	desde = Number(desde);
@@ -43,7 +44,7 @@ app.get('/usuario', function (req, res) {
 })
 
 
-app.post('/usuario', function (req, res) {
+app.post('/usuario', [verificaToken, verificaAdminRole], function (req, res) {
 	let body = req.body;
 
 	let usuario = new Usuario({
@@ -69,7 +70,7 @@ app.post('/usuario', function (req, res) {
 })
 
 
-app.put('/usuario/:id', function (req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdminRole], function (req, res) {
 	let id = req.params.id;
 	let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
 
@@ -89,7 +90,7 @@ app.put('/usuario/:id', function (req, res) {
 })
 
 
-app.delete('/usuario/:id', function (req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdminRole], function (req, res) {
 	let id = req.params.id;
 
 	// Usuario.findByIdAndRemove( id, (err, usuarioBorrado)=>{
