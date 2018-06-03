790b6df156154f1376359b0ebe8d066b8bb434eb
MiguelFs86 <miguel.fuentes.sobrin@gmail.com>
2018-06-03 11:47:44 +0200

    Fix mLab password

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev'){
 	urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-	urlDB = 'mongodb://cafe-user:123456@ds247270.mlab.com:47270/udemy-nodejs-cafe'
+	urlDB = 'mongodb://cafe-user:qwerty1234@ds247270.mlab.com:47270/udemy-nodejs-cafe'
 }
 
 process.env.URLDB = urlDB;
